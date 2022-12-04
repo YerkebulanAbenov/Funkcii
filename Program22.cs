493a448ae0795f555c61541bd77af1a213306e8d
YerkebulanAbenov <118619322+YerkebulanAbenov@users.noreply.github.com>
2022-12-04 12:19:08 +0600

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Prompt (string message)
+{
+    Console.WriteLine($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Vvedite chislo");
+int start = 1;
+while (start <= number)
+{
+    int cube = start * start * start;
+    System.Console.WriteLine(cube);
+    start += 1;
+}
